bd27d0c9c8a6b36cb43bb3e1da299918a7bb32b8
Daniel <daniel.repres@hotmail.com>
2021-11-27 19:06:04 -0300

    Atualizado

--- src/schema.sql
+++ src/schema.sql
@@ -10,20 +10,3 @@ create table if not exists usuarios (
 	cpf text unique,
 	tel text
 );
-
-drop table if exists clientes;
-
-create table if not exists clientes (
-	id serial primary key,
-	usuario_id integer not null,
-	nome text not null,
-	cpf integer not null unique,
-	telefone text not null,
-	enderenço text,
-	complemento text,
-	CEP text,
-	Bairro text,
-	Cidade text not null,
-	UF text not null,
-	foreign key (usuario_id) references usuarios (id)
-);
